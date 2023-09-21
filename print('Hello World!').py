04a13aece72a09af7500c2ab716f80fa750e3886
Openiuk <openyusha@gmail.com>
2023-09-21 18:21:11 +0200

    work with class

--- print('Hello World!').py
+++ print('Hello World!').py
@@ -4,3 +4,22 @@ if name and age >=18:
     print('alles gut')
 else:
     print('try again')
+
+
+class Animals:
+
+    def __init__(self, name, age, rod) -> None:
+        self.name = name
+        self.age = age
+        self.rod = rod
+
+
+class Cat(Animals):
+
+    def say_someone():
+        return 'cat say meou'
+    
+
+cat = Cat('Tom', 6, 'kot')
+
+print(cat.say_someone)
